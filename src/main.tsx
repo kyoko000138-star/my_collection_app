8c5a60f36342ae09b50da0cecc59e1b2b39242e3
kyoko000138-star <kyoko000138@gmail.com>
2025-12-11 21:36:53 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
-// src/main.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+// BrowserRouter 대신 HashRouter를 가져옵니다.
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    {/* HashRouter로 감싸면 주소에 #이 붙으면서 404 에러가 사라집니다 */}
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
